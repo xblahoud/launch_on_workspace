e0f36d326ce50fc65599d258681661c4008c38c6
Fanda Blahoudek <fandikb@gmail.com>
2019-11-14 08:33:22 -0600

    update consumption MDPs with TeXStudio & git

--- consMDP.py
+++ consMDP.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from launcher import *
 
 port = 8899
@@ -6,5 +8,14 @@ homedir = "/home/fblahoudek/tools/consumption-MDP"
 
 ws = workspace - 1
 
+terminal(ws, homedir, command="bash -c 'git pull; bash'", new_win_name="consMDP: git")
 jupyter_lab(ws, homedir, "consMDP", port)
-terminal(ws, homedir, new_win_name="consMDP: git")
+
+tex_dir = "/home/fblahoudek/research/consumption-mdp-ltl/"
+tex_file = f"{tex_dir}/notes.tex"
+workspace = 6
+ws = workspace -1
+texstudio(ws, tex_file)
+terminal(ws, tex_dir, command="bash -c 'git pull; pdflatex notes.tex; pdflatex notes.tex; bash'")
+
+launch_and_move(['evince',f'{tex_file[:-3]}pdf'], ws)
